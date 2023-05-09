08cb269609c675ae0af06aead26a91456a58639e
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-05-10 00:54:38 +1000

    add custom prompts to append to user prompts, minimize user effort

--- gpterbot.py
+++ gpterbot.py
@@ -7,25 +7,39 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 system = "System: "
 user = "You: "
-welcome_msg = """Welcome! I am Jupyter Bot. Your personal Jupyter Notebook assistant. 
+start_instruction = "\nTo start working through these step one by one. Type 'Start'"
+welcome_instruction = """Welcome! I am Jupyter Bot. Your personal Jupyter Notebook assistant. 
 To get started. Please provide me the context of your notebook and the datasets.
 Here is an example:
-The notebook I am writing is about ... . These are the given dataset:
+The notebook I am writing is about .... These are the given dataset:
 games.csv - metadata about games
 turns.csv - all turns from start to finish of each game
+
+To end type '$'.
 """
+
 log = [
-    {"role": "system", "content": "You are a Jupyter Notebook code assistant for data scientists."}
-]
+    {"role": "system", "content": "You are a Jupyter Notebook code assistant for data scientists. Follow the instruction carefully"}
+] 
+custom_prompts = []
+more_detail_prompt = """\nAsk me to provide you with more detail about each dataset e.g., Can you please provide more detail about each dataset and their variables? Do not generate extra informations."""
+goal_prompt = """\nAsk me what my goal is e.g., What is your goal for this task based on the data provided?"""
+general_step_prompt = """\nGenerate the required step that I can take as detail as possible. Do not generate any code."""
+custom_prompts.append(more_detail_prompt)
+custom_prompts.append(goal_prompt)
+custom_prompts.append(general_step_prompt)
 
 
 def run():
-    print(f"{system}{welcome_msg}")
+    num_prompts = 0
+    print(f"{system}{welcome_instruction}")
     while True:
         user_prompt = input(f"{user}")
         # stop the program
         if user_prompt == '$':
             break
+        if num_prompts < len(custom_prompts):   
+            user_prompt += custom_prompts[num_prompts]
         # append the user input to the log
         log.append({"role": "user", "content": f"{user_prompt}"},)
         response = openai.ChatCompletion.create(
@@ -33,10 +47,9 @@ def run():
             messages=log
         )
         answer = response.choices[0].message.content
+        # append the gpt answer to the log
         log.append({"role": "assistant", "content": f"{answer}"},)
+        if num_prompts == len(custom_prompts)-1:
+            answer += start_instruction
+        num_prompts += 1
         print(f"{system}{answer}\n")
-    print(log)
-
-
-if __name__ == "__main__":
-    run()
