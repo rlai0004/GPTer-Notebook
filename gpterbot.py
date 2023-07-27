b43c7810ee8694921adb4ff024a28036beab6bfd
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-07-27 14:06:51 +1000

    improve prompts

--- gpterbot.py
+++ gpterbot.py
@@ -6,34 +6,30 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 system = "System: "
-
 user = "You: "
-
 welcome_instruction = """Welcome! I am Jupyter Bot. Your personal Jupyter Notebook assistant. 
 To get started. Please provide me the context of your notebook and the datasets.
 Here is an example:
 The notebook I am writing is about .... These are the given dataset:
 games.csv - metadata about games
 turns.csv - all turns from start to finish of each game
-
 To end type '$'.
 """
-
-start_instruction = """\nTo start. Type the step number. Or type 'start' to start from step 1 and type 'continue' to keep going. 
-"""
-
+persona = "You are an experienced data scientist named Sarah, specializing in Jupyter Notebook. " \
+"Your role is to expertly guide users in creating their notebooks, providing step-by-step assistance " \
+"while carefully following their instructions to ensure a smooth and successful notebook creation process."
 log = [
-    {"role": "system", "content": "You are a Jupyter Notebook code assistant for data scientists. Follow the instruction carefully"}
+    {"role": "system", "content": persona}
 ]
-
 custom_prompts = []
-more_detail_prompt = """\nAsk me to provide you with more detail about each dataset e.g., Can you please provide more detail about each dataset and their variables? Do not generate extra informations."""
-goal_prompt = """\nAsk me what my goal is e.g., What is your goal for this task based on the data provided?"""
-general_step_prompt = """\nGenerate the required step to achieve my goal as detail as possible. Do not generate any code."""
+more_detail_prompt = """\nAsk the user to provide you with more detail about each dataset e.g., Can you please provide more detail about each dataset and their variables? Do not generate extra informations."""
+goal_prompt = """\nAsk the user what their goal is e.g., What is your goal for this task based on the data provided?"""
+general_step_prompt = """\nGenerate the required step to achieve the user's goal as detail as possible. Do not generate any code."""
 custom_prompts.append(more_detail_prompt)
 custom_prompts.append(goal_prompt)
 custom_prompts.append(general_step_prompt)
 
+start_instruction = """\nTo start. Type the step number. Or type 'start' to start from step 1 and type 'continue' to keep going.\n"""
 
 def run():
     current_step = 0
@@ -65,3 +61,4 @@ def run():
         num_prompts += 1
         print(f"{system}{answer}\n")
 
+run()
